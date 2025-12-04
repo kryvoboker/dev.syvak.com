d0cb37a66f7bc2663e64e798dcbfe10c56741cf2
kamaz <kryvoboker@gmail.com>
2025-12-04 13:53:06 +0200

    update docker configs and app.php

--- httpdocs/app/bootstrap/app.php
+++ httpdocs/app/bootstrap/app.php
@@ -27,12 +27,12 @@ $app = Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->alias([
-            'localization.parameter' => LocalizationByParameter::class,
-            'localization.redirect'  => LocalizationByParameterWithRedirect::class,
             'localization.header'    => LocalizationByHeader::class,
             'localization.cookie'    => LocalizationByCookie::class,
             'localization.session'   => LocalizationBySession::class,
             'localization.model'     => LocalizationByModel::class,
+            'localization.parameter' => LocalizationByParameter::class,
+            'localization.redirect'  => LocalizationByParameterWithRedirect::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
