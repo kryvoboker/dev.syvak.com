43f485291a61219428eea7ee8e1850c1cd649353
kamaz <kryvoboker@gmail.com>
2026-03-24 14:40:35 +0200

    refactor category view

---
+++ app-code/app/resources/views/catalog/pages/partials/category/products-filter-btn.blade.php
@@ -0,0 +1,8 @@
+<button class="open-category-filter-drawer-btn default-btn border border-opacity-light-gray-40% px-3 py-1.5 text-11px
+               uppercase tracking-0.04em duration-200 ease-in-out hover:border-white md:text-sm lg:text-base"
+        type="button"
+        aria-haspopup="dialog"
+        aria-expanded="false"
+        aria-controls="category-filter-drawer">
+    ФІЛЬТР
+</button>
---
+++ app-code/app/resources/views/catalog/pages/partials/category/products-filter-content.blade.php
@@ -0,0 +1,22 @@
+<div class="overlay overlay-open:translate-x-0 drawer drawer-start hidden category-filter-drawer bg-black"
+     id="category-filter-drawer"
+     role="dialog"
+     tabindex="-1">
+    <div class="drawer-header flex-col items-stretch px-4 pt-38px pb-6">
+        <button type="button"
+                class="btn btn-text btn-circle ms-auto mb-4 size-8"
+                aria-expanded="true"
+                aria-controls="Close category filter drawer"
+                data-overlay="#category-filter-drawer">
+            <span class="icon-[iconamoon--close] custom-icon size-8"></span>
+        </button>
+
+        <h4>Фільтр</h4>
+    </div>
+
+    <div class="drawer-body px-4">
+        <p class="text-light-gray text-sm uppercase tracking-0.04em md:text-base">
+            Фільтр товарів буде тут
+        </p>
+    </div>
+</div>
