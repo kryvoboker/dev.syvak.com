6ff0657af9850321c0106110fe7b5eb0585310f4
kamaz <kryvoboker@gmail.com>
2026-03-25 11:24:35 +0200

    dev load more btn for category page and refactor category page

---
+++ app-code/app/resources/views/catalog/pages/partials/category/load-more-btn.blade.php
@@ -0,0 +1,12 @@
+@if($category_page_settings['is_ajax_products_loading_enabled'])
+    <div class="flex justify-center mt-18px lg:mt-6 xl:mt-8">
+        <button class="default-btn flex items-center justify-center gap-x-2 uppercase tracking-0.04em text-white duration-200 ease-in-out hover:text-light-gray"
+                type="button">
+            <span>
+                {{ __('catalog/default.buttons.show_more') }}
+            </span>
+
+            <span class="icon-[solar--arrow-down-linear] custom-icon"></span>
+        </button>
+    </div>
+@endif
