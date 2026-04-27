9eb08e3a926d159b25e3c29203052fac792b397d
kamaz <kryvoboker@gmail.com>
2026-04-27 17:46:45 +0300

    fix args/vars types for getAppParams func

--- app-code/app/resources/assets/catalog/ts/shared/lib/getAppParam.ts
+++ app-code/app/resources/assets/catalog/ts/shared/lib/getAppParam.ts
@@ -1,9 +1,9 @@
-export function getAppParam<T>(key: string): T | null {
+export function getAppParam<T>(key: string | number): T | null {
     if (typeof window.app_params !== 'object' || window.app_params === null) {
         return null;
     }
 
-    const value = window.app_params[key];
+    const value: unknown | any | undefined = window.app_params[key];
 
     return value === undefined ? null : value as T;
 }
