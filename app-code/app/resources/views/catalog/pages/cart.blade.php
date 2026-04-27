9a1d7ffd50b24d79e100a47bf04c670544234849
kamaz <kryvoboker@gmail.com>
2026-04-27 17:48:56 +0300

    view for cart page for functionalyti for CRUD operations with prods for cart

--- app-code/app/resources/views/catalog/pages/cart.blade.php
+++ app-code/app/resources/views/catalog/pages/cart.blade.php
@@ -1,13 +1,28 @@
 @extends('catalog.layouts.main')
 
 @section('content')
+    <script>
+        window.app_params = {
+            ...(window.app_params ?? {}),
+            ...@js([
+                'cart_mode' => 'regular',
+                'cart_modal_ajax_url' => localized_route('localized.catalog.cart-modal-ajax.index'),
+                'cart_store_url' => localized_route('localized.catalog.cart.store'),
+                'cart_update_url_pattern' => localized_route('localized.catalog.cart.update', ['cart_id' => '__variant_id__']),
+                'cart_delete_url_pattern' => localized_route('localized.catalog.cart.delete', ['cart_id' => '__variant_id__']),
+                'order_validate_url' => localized_route('localized.catalog.order-confirm.validate'),
+                'order_store_url' => localized_route('localized.catalog.order-confirm.store'),
+            ])
+        };
+    </script>
+
     <x-catalog::common.breadcrumbs
         :breadcrumbs="$breadcrumbs"
     />
 
     <section class="cart section" id="cart">
-        <div class="container">
-
+        <div class="container flex flex-col gap-4" id="cart-page-root">
+            @include('catalog.partials.cart.page-content', ['cart_data' => $cart_data ?? []])
         </div>
     </section>
 @endsection
