83d851291c6e7b91469c3dbaed9ec1545fd5d2e4
Roman Kryvobok <fast.kamaz@gmail.com>
2026-06-30 13:52:29 +0300

    add CheckForbiddenIpMeddleware class

---
+++ app-code/app/app/Http/Middleware/CheckForbiddenIpMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckForbiddenIpMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $ip = $request->ip();
+        $forbidden_ips = config('ip.forbidden_list', []) |> trim_strs_in_arr(...);
+
+        if (in_array($ip, $forbidden_ips, true)) {
+            abort(403, 'Access denied for this site!');
+        }
+
+        return $next($request);
+    }
+}
