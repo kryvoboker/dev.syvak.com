91230207be9423f4d1d4323ed1472f45af913c50
Roman Kryvobok <fast.kamaz@gmail.com>
2026-07-10 15:26:15 +0300

    remove redundant import for Log class

--- app-code/app/app/Http/Controllers/Pages/CheckoutController.php
+++ app-code/app/app/Http/Controllers/Pages/CheckoutController.php
@@ -17,7 +17,6 @@ use App\Services\HeaderService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Arr;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 use Illuminate\View\View;
 use Modules\NovaPoshta\Services\NovaPoshtaCheckoutDataService;
